e200d2a42cad1e09f214d178d0211a5256264c37
Pankaj Padekar <padekarpankaj@gmail.com>
2023-09-30 17:16:12 +0530

    Working

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ Search_input = input("Search :- ")
 cap_seach = string.capwords(Search_input)
 lists = cap_seach.split()
 word = "_".join(lists)
+
+url ="https://n.wikipedia.org/wiki/"+word
+
+def wikiSearch(url):
+    url_link =requests.get(url)
+    soup = BeautifulSoup(url_link.content, 'html.parser')
+    details_info = soup('table' , {'class':'infobox'})
+    for i in details_info:
