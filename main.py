6623b674f8c63b28e469c24d11a4770b405e8da4
Pankaj Padekar <padekarpankaj@gmail.com>
2023-09-30 17:17:40 +0530

    Working

--- main.py
+++ main.py
@@ -7,10 +7,23 @@ cap_seach = string.capwords(Search_input)
 lists = cap_seach.split()
 word = "_".join(lists)
 
-url ="https://n.wikipedia.org/wiki/"+word
+url ="https://en.wikipedia.org/wiki/"+ word
 
 def wikiSearch(url):
-    url_link =requests.get(url)
+    url_link = requests.get(url)
     soup = BeautifulSoup(url_link.content, 'html.parser')
     details_info = soup('table' , {'class':'infobox'})
     for i in details_info:
+        h = i.find_all('tr')
+        for j in h:
+            heading = j.find_all('th')
+            detail = j.find_all('td')
+            if heading is not None and detail is not None:
+                for x,y in zip(heading,detail):
+                    print(f"{x.text} :: {y.text}")
+                    print("------------------")
+
+    for i in range(1,10):
+        print(soup('p')[i].text)
+
+wikiSearch(url)
