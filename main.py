29da3cf1c44b9c923a7bdb3d70f96219da0fc08c
Pankaj Padekar <padekarpankaj@gmail.com>
2023-09-30 16:58:34 +0530

    made changes

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import requests
 import string
 from bs4 import BeautifulSoup
 
-Search_input =
+Search_input = input("Search :- ")
+cap_seach = string.capwords(Search_input)
+lists = cap_seach.split()
+word = "_".join(lists)
