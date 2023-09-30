b059b90b67a4ba4f884db358bd58cd85b975c820
Pankaj Padekar <padekarpankaj@gmail.com>
2023-09-30 17:20:42 +0530

    Working

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import string
 from bs4 import BeautifulSoup
 
+# Website = "https://en.wikipedia.org/wiki/"
+
 Search_input = input("Search :- ")
 cap_seach = string.capwords(Search_input)
 lists = cap_seach.split()
